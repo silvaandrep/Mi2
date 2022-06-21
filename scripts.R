d51430d6a1f0dd38a95ce0b0fa408f0d48257dbe
André Pereira da Silva <silva.andrep@gmail.com>
2022-06-20 21:35:46 -0300

    Arquivo scripts. R

---
+++ scripts.R
@@ -0,0 +1,3 @@
+precipitacaoDiaria <- read_html("http://old.apac.pe.gov.br/meteorologia/chuvas-rmr.php")
+precipitacaoMensal <- read_html("http://old.apac.pe.gov.br/meteorologia/monitoramento-pluvio.php")
+mediaMensal <- read_html("https://www.apac.pe.gov.br/193-climatologia/521-climatologia-por-municipio")
